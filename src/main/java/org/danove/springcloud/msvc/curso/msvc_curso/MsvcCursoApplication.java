9d1a60e703eefccee20c2d183e513885bb78e485
Dayron Novo Verde <danove@pop-os.localdomain>
2023-05-25 22:44:34 -0400

    Bases creadas

---
+++ src/main/java/org/danove/springcloud/msvc/curso/msvc_curso/MsvcCursoApplication.java
@@ -0,0 +1,13 @@
+package org.danove.springcloud.msvc.curso.msvc_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/danove/springcloud/msvc/curso/msvc_curso/MsvcCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danove.springcloud.msvc.curso.msvc_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
